54362cf5711ba29edc124b02c7f047207fbcfd85
kadzima <35219659+adzimak@users.noreply.github.com>
2021-11-03 17:10:46 -0600

    New commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 var dateObj = new Date();
- // months from 1-12
+var month = dateObj.getUTCMonth(); // months from 1-12
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August","September", "October", "November", "December"];
 var day = dateObj.getUTCDate();
-var month = dateObj.getUTCMonth();
 var year = dateObj.getUTCFullYear();
 
 document.getElementById("currentdate").innerHTML = days[dateObj.getDay()] + ',' + day + ' ' + monthNames[dateObj.getMonth()] + ' ' + year;
